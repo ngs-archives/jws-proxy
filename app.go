2a412a09084d35bf42a90724ed5ac194bd085a98
Atsushi Nagase <a@ngs.io>
2017-01-28 16:47:16 +0900

    Initial import

---
+++ app.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/rs/cors"
+)
+
+func main() {
+	handler := cors.Default().Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		url := r.URL
+		url.Scheme = "http"
+		url.Host = "jws.jalan.net"
+		q := url.Query()
+		q.Set("key", os.Getenv("JWS_API_KEY"))
+		url.RawQuery = q.Encode()
+		res, err := http.Get(url.String())
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		w.Header().Set("Content-Type", "text/xml")
+		io.Copy(w, res.Body)
+	}))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(":"+port, handler)
+}
